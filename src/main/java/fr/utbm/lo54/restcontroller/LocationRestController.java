8a183a42ed8150100fe398ea5c3e470cd6e36540
EdemosBonboy <yovoedem33@gmail.com>
2019-05-25 12:51:19 +0200

    Rest webservice for Location

--- src/main/java/fr/utbm/lo54/restcontroller/LocationRestController.java
+++ src/main/java/fr/utbm/lo54/restcontroller/LocationRestController.java
@@ -5,8 +5,9 @@
  */
 package fr.utbm.lo54.restcontroller;
 
-import fr.utbm.lo54.entity.Client;
-import fr.utbm.lo54.service.IClientService;
+import fr.utbm.lo54.entity.Location;
+import fr.utbm.lo54.service.ILocationService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author edemos
  */
 @RestController
-@RequestMapping("/api/client")
-public class ClientRestController {
+@RequestMapping("/api/location")
+public class LocationRestController {
     
     @Autowired
-    private IClientService clientService;
+    private ILocationService locationService;
     
-    @RequestMapping(value = "/{id}/show", method = RequestMethod.GET)
-    public Client show(@PathVariable(value = "id") Integer id) {
-        return clientService.getById(id);
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Location> index() {
+        return locationService.listAll();
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Location get(@PathVariable(value = "id") Integer id) {
+        return locationService.getById(id);
     }
     
 }
