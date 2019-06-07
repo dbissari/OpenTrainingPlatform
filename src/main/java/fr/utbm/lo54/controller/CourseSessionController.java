678b464ed5bbc81143c45d86def538f513cfd809
Serge Bissari <dbissari@gmail.com>
2019-06-07 10:32:08 +0200

    Started using DTO for course session

--- src/main/java/fr/utbm/lo54/controller/CourseSessionController.java
+++ src/main/java/fr/utbm/lo54/controller/CourseSessionController.java
@@ -5,6 +5,7 @@
  */
 package fr.utbm.lo54.controller;
 
+import fr.utbm.lo54.dto.CourseSessionDto;
 import fr.utbm.lo54.entity.Client;
 import fr.utbm.lo54.entity.CourseSession;
 import fr.utbm.lo54.service.ICourseService;
@@ -14,8 +15,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
@@ -35,103 +38,116 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/course-session")
 public class CourseSessionController {
-    
+
     private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-    
+
     @Autowired
     private ICourseSessionService courseSessionService;
-    
+
     @Autowired
     private ICourseService courseService;
-    
+
     @Autowired
     private ILocationService locationService;
-    
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(Date.class, new CustomDateEditor(DATE_FORMAT, true));
     }
-    
+
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(ModelMap model) {
-        model.addAttribute("courseSessions", courseSessionService.listAll());
-        
+        model.addAttribute("courseSessions", courseSessionService.listAll().stream()
+                .map(post -> convertToDto(post))
+                .collect(Collectors.toList()));
+
         return "course_session/index";
     }
-    
+
     @RequestMapping(value = "/new", method = RequestMethod.GET)
     public String showNewForm(ModelMap model) {
         model
                 .addAttribute("courseSession", new CourseSession())
                 .addAttribute("courses", courseService.listAll())
                 .addAttribute("locations", locationService.listAll());
-        
+
         return "course_session/form";
     }
-    
+
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public String submitNewForm(@Valid @ModelAttribute("courseSession") CourseSession courseSession, BindingResult result) {
         if (result.hasErrors()) {
             return "course_session/form";
         }
-        
+
         courseSessionService.create(courseSession);
-        
+
         return "redirect:/course-session";
     }
-    
+
     @RequestMapping(value = "/{id}/details", method = RequestMethod.GET)
     public String showDetails(@PathVariable("id") Integer id, ModelMap model) {
         CourseSession courseSession = courseSessionService.getById(id);
         List<Client> attendees = courseSessionService.listAllAttendeesByCourseSession(courseSession);
-        Double fillingPercentage = 100 * (double)attendees.size() / (double)courseSession.getMax();
+        Double fillingPercentage = 100 * (double) attendees.size() / (double) courseSession.getMax();
         model
                 .addAttribute("courseSession", courseSession)
                 .addAttribute("attendees", attendees)
                 .addAttribute("fillingPercentage", fillingPercentage);
-        
+
         return "course_session/details";
     }
-    
+
     @RequestMapping(value = "/{id}/register", method = RequestMethod.GET)
     public String showRegisterForm(@PathVariable("id") Integer id, ModelMap model) {
         CourseSession courseSession = courseSessionService.getById(id);
         model
                 .addAttribute("courseSession", courseSession)
                 .addAttribute("client", new Client());
-        
+
         return "course_session/register";
     }
-    
+
     @RequestMapping(value = "/{id}/register", method = RequestMethod.POST)
     public String submitRegisterForm(@PathVariable("id") Integer id, @Valid @ModelAttribute("client") Client client, BindingResult result) {
         if (result.hasErrors()) {
             return "course_session/register";
         }
-        
+
         CourseSession courseSession = courseSessionService.getById(id);
         courseSessionService.register(courseSession, client);
-        
+
         return "redirect:/course-session/" + id + "/details";
     }
-    
+
     @RequestMapping(value = "/search", method = {RequestMethod.GET, RequestMethod.POST})
     public String showSearch(ModelMap model, HttpServletRequest request) throws ParseException {
         String courseKeyword = request.getParameter("course_keyword");
-        Date date = request.getParameter("date") == null || request.getParameter("date").equals("") ?
-                null : DATE_FORMAT.parse(request.getParameter("date"));
-        Integer locationId = request.getParameter("location_id") == null || request.getParameter("location_id").equals("") ?
-                null : Integer.valueOf(request.getParameter("location_id"));
-        
+        Date date = request.getParameter("date") == null || request.getParameter("date").equals("")
+                ? null : DATE_FORMAT.parse(request.getParameter("date"));
+        Integer locationId = request.getParameter("location_id") == null || request.getParameter("location_id").equals("")
+                ? null : Integer.valueOf(request.getParameter("location_id"));
+
         List<CourseSession> courseSessions = courseSessionService.listAllByCourseDateAndLocation(courseKeyword, date, locationId);
-        
+
         model
-                .addAttribute("courseSessions", courseSessions)
+                .addAttribute("courseSessions", courseSessions.stream()
+                        .map(post -> convertToDto(post))
+                        .collect(Collectors.toList()))
                 .addAttribute("locations", locationService.listAll())
                 .addAttribute("courseKeyword", courseKeyword)
                 .addAttribute("date", date != null ? DATE_FORMAT.format(date) : null)
                 .addAttribute("locationId", locationId);
-        
+
         return "course_session/search";
+    }
+
+    private CourseSessionDto convertToDto(CourseSession courseSession) {
+        CourseSessionDto courseSessionDto = modelMapper.map(courseSession, CourseSessionDto.class);
+        courseSessionDto.computeFillingPercentage();
+        return courseSessionDto;
     }
 }
---
+++ src/main/java/fr/utbm/lo54/dto/CourseSessionDto.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.utbm.lo54.dto;
+
+import fr.utbm.lo54.entity.Client;
+import fr.utbm.lo54.entity.Course;
+import fr.utbm.lo54.entity.Location;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author bright
+ */
+public class CourseSessionDto implements Serializable {
+    
+    private Integer id;
+    
+    private Date startDate;
+    
+    private Date endDate;
+    
+    private Integer max;
+    
+    private Double fillingPercentage;
+    
+    private Course course;
+    
+    private Location location;
+    
+    private List<Client> attendees;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setMax(Integer max) {
+        this.max = max;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public List<Client> getAttendees() {
+        return attendees;
+    }
+    
+    public void setAttendees(List<Client> attendees) {
+        this.attendees = attendees;
+    }
+    
+    public void computeFillingPercentage() {
+        this.fillingPercentage = 100 * (double) this.attendees.size() / (double) this.max;
+    }
+}
